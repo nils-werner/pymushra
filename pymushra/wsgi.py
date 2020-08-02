24ed48e07743a0f90562f7ddc992358a474e4f15
Nils Werner <nils@hey.com>
2020-08-02 11:50:57 +0200

    Fix some paths

--- pymushra/wsgi.py
+++ pymushra/wsgi.py
@@ -5,5 +5,5 @@ from pymushra.service import app as application
 application.config['webmushra_dir'] = os.path.join(os.getcwd(), "webmushra")
 
 application.config['db'] = TinyDB(
-    os.path.join(os.getcwd(), "../db/webmushra.json")
+    os.path.join(os.getcwd(), "db/webmushra.json")
 )
